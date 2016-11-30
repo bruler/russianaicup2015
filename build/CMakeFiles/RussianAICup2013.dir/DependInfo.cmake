8defa424b48238d11da6174b53ac05e1b6c33b77
daemonSlayer <kpgupta98@gmail.com>
2016-11-30 13:06:28 +0530

    Initial commit

---
+++ build/CMakeFiles/RussianAICup2013.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/MyStrategy.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/MyStrategy.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/RemoteProcessClient.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/RemoteProcessClient.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/Runner.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/Runner.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/Strategy.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/Strategy.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/csimplesocket/ActiveSocket.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/csimplesocket/ActiveSocket.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/csimplesocket/HTTPActiveSocket.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/csimplesocket/HTTPActiveSocket.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/csimplesocket/PassiveSocket.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/csimplesocket/PassiveSocket.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/csimplesocket/SimpleSocket.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/csimplesocket/SimpleSocket.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/Bonus.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/Bonus.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/Car.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/Car.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/CircularUnit.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/CircularUnit.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/Game.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/Game.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/Move.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/Move.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/OilSlick.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/OilSlick.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/Player.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/Player.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/PlayerContext.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/PlayerContext.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/Projectile.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/Projectile.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/RectangularUnit.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/RectangularUnit.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/Unit.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/Unit.cpp.o"
+  "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/src/model/World.cpp" "/home/cortana/Projects/CppProjects/Russian AI Cup/russianaicup2015/build/CMakeFiles/RussianAICup2013.dir/src/model/World.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/RussianAICup2013.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RussianAICup2013.dir/src/csimplesocket/ActiveSocket.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/csimplesocket/HTTPActiveSocket.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/csimplesocket/PassiveSocket.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/csimplesocket/SimpleSocket.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/Bonus.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/Car.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/CircularUnit.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/Game.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/Move.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/OilSlick.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/Player.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/PlayerContext.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/Projectile.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/RectangularUnit.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/Unit.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/model/World.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/MyStrategy.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/RemoteProcessClient.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/Runner.cpp.o"
+  "CMakeFiles/RussianAICup2013.dir/src/Strategy.cpp.o"
+  "RussianAICup2013.pdb"
+  "RussianAICup2013"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RussianAICup2013.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
